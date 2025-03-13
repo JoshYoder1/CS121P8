2a1b90623c935696460b9c7fbbcd4d6252afea98
JoshYoder1 <josh.yoder@bsu.edu>
2025-03-13 15:55:16 +0000

    edit main

--- Main.java
+++ Main.java
@@ -4,40 +4,10 @@
 import java.util.*;
 
 public class Main{
-	static void humanGuesser(){
-		System.out.print("human guesser");
-		int answer = (int)(Math.random() * 101);
-		boolean keepGoing = true;
-		String guess = "12";
-		int counter = 1;
-		
-		while (keepGoing){
-			System.out.print(counter + ") Please enter a number: ");
-		                   	Scanner input = new Scanner(System.in);
-			guess = input.nextLine();
-			System.out.println();
-			if(Integer.parseInt(guess) == answer){
-				System.out.println("got it!");
-				keepGoing = false;
-			}//end if
-			else if(Integer.parseInt(guess) < answer){
-				System.out.println("too low");
-			}//end if
-			else if(Integer.parseInt(guess) > answer){
-				System.out.println("too high");
-			}//end if
-			else {
-				System.out.println("Invalid Answer");
-
-			}// end else
-			counter = counter + 1;
-		}//end while
-	}// end humanGuesser
-	
 	public static void main(String[] args) {
 		boolean keepGoing = true;
-		Scanner input = new Scanner(System.in);
 		while (keepGoing){
+			Scanner input = new Scanner(System.in);
 			System.out.println("0) Exit");
 			System.out.println("1) Human Guesser");
 			System.out.println("2) Computer Guesser");
@@ -48,6 +18,39 @@ public class Main{
 			}//end if
 			else if(choice.equals("1")){
 				humanGuesser();
+			}
 		}//end while
+		input.close();
 	}//end main
+	
+	public static void humanGuesser(){
+		System.out.print("human guesser");
+		int answer = (int)(Math.random() * 101);
+		boolean keepGoing = true;
+		int guess;
+		int counter = 1;
+		
+		while (keepGoing){
+			System.out.print(counter + ") Please enter a number: ");
+			guess = Integer.parseInt(input.nextLine());
+			System.out.println();
+			if(guess == answer){
+				System.out.println("got it!");
+				keepGoing = false;
+			}//end if
+			else if(guess < answer){
+				System.out.println("too low");
+			}//end if
+			else if(guess > answer){
+				System.out.println("too high");
+			}//end if
+			else {
+				System.out.println("Invalid Answer");
+
+			}// end else
+			counter = counter + 1;
+		}//end while
+	}// end humanGuesser
+	
+	
 }//end class

80ffb221f22ba915e8d120e9e98b3c42c0554310
JoshYoder1 <josh.yoder@bsu.edu>
2025-03-13 15:36:33 +0000

    edit main

--- Main.java
+++ Main.java
@@ -13,35 +13,41 @@ public class Main{
 		
 		while (keepGoing){
 			System.out.print(counter + ") Please enter a number: ");
-			Scanner input = new Scanner(System.in);
-			guess = input.nextline();
+		                   	Scanner input = new Scanner(System.in);
+			guess = input.nextLine();
 			System.out.println();
-			if(int.class(guess) == answer){
+			if(Integer.parseInt(guess) == answer){
 				System.out.println("got it!");
 				keepGoing = false;
 			}//end if
-			else if(int.class(guess) < answer){
+			else if(Integer.parseInt(guess) < answer){
 				System.out.println("too low");
 			}//end if
-			else if(int.class(guess) > answer){
+			else if(Integer.parseInt(guess) > answer){
 				System.out.println("too high");
 			}//end if
+			else {
+				System.out.println("Invalid Answer");
+
+			}// end else
 			counter = counter + 1;
 		}//end while
 	}// end humanGuesser
 	
 	public static void main(String[] args) {
 		boolean keepGoing = true;
-		Scanner choice = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		while (keepGoing){
 			System.out.println("0) Exit");
-			Systen.out.println("1) Human Guesser");
+			System.out.println("1) Human Guesser");
 			System.out.println("2) Computer Guesser");
-			String choice = input.nextline();
+			String choice = input.nextLine();
 			if(choice.equals("0")){
 				System.out.println("Goodbye!");
 				keepGoing = false;
 			}//end if
+			else if(choice.equals("1")){
+				humanGuesser();
 		}//end while
 	}//end main
 }//end class
